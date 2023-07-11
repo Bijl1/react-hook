2da77957bfba2596a693ff38d6e26588b8010f54
Jonathan Bijl <sannoayo@gmail.com>
2023-07-11 07:26:25 -0400

    updated app and add timertwo.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import "./App.css";
 import { useState } from "react";
 import Counter from "./components/01-use-state/Counter";
+import Timer from "./components/02-use-state-loop/Timer";
  
 function App() {
   const [show, setShow] = useState(true);
