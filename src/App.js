86792db4276d3d63c4d8078b1443f3ec73a71709
Jonathan Bijl <sannoayo@gmail.com>
2023-07-10 22:05:09 -0400

    Counter component and render it in App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css";
+import { useState } from "react";
+import Counter from "./components/01-use-state/Counter";
+ 
 function App() {
+  const [show, setShow] = useState(true);
+ 
   return (
-    <div className="App"></div>
+    <div className="App">
+      <button onClick={() => setShow(!show)}>
+        { show ? "Hide" : "Show"}
+      </button>
+      {show && <Counter />}
+    </div>
   );
 }
-
+ 
 export default App;
